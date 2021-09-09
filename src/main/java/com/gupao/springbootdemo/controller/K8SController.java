27ee4f49584b95b9f48aafa8288347defc13fd3c
syp356 <1440825043@qq.com>
2021-09-09 23:34:32 +0800

    测试

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -8,7 +8,6 @@ import java.net.InetAddress;
 @RestController
 public class K8SController {
 
-
     @RequestMapping("/k8s")
     public String k8s(){
         return "hello   K8s <br/>111222 xingbxing  ";
